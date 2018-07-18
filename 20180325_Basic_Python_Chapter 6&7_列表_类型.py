4c5640c6f3c76cc0c993829f3eec2d0cb2d2d968
Yiwen Xu <37330367+XUSushi@users.noreply.github.com>
2018-07-19 00:12:54 +0800

    Basic Python Exercise Chapter 6&7

--- 20180325_Basic_Python_Chapter 6&7_列表_类型.py
+++ 20180325_Basic_Python_Chapter 6&7_列表_类型.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Mar 25 11:22:58 2018
 
-@author: 许某某
+@author: 许逸文
 """
 import random
 
@@ -29,3 +29,19 @@ print(c)#列表中的元素可以重复多次
 b=random.sample(c,random.randint(0,len(c)))
 print(b) 
 print(c*3)
+
+#排序
+a = [10, 1, 11, 13, 11, 2]
+a.sort()
+print (a)
+
+#如果不想改变原来列表中的值，可以使用 sorted 函数
+a = [10, 1, 11, 13, 11, 2]
+b = sorted(a)
+print （a）
+print （b）
+
+#reverse() 会将列表中的元素从后向前排列。
+a = [1, 2, 3, 4, 5, 6]
+a.reverse()
+print (a)
